cd9a7e8b213de5388e7f391527ccd96e73a4e35d
Jeff Minnear <jeffminnear@gmail.com>
2021-02-26 12:20:23 -0700

    Add character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 6f;
+    [SerializeField]
+    private float gravity = 1f;
+    [SerializeField]
+    private float jumpHeight = 15f;
+    private CharacterController controller;
+    private float yVelocity;
+    private int maxJumps = 2;
+    private int jumps = 0;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0f, 0f);
+        Vector3 velocity = direction * speed;
+
+        if (controller.isGrounded)
+        {
+            jumps = 0;
+
+            if (IsJumpPressed())
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            if (IsJumpPressed() && jumps < maxJumps)
+            {
+                Jump();
+            }
+            yVelocity -= gravity;
+        }
+
+        velocity.y = yVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private bool IsJumpPressed()
+    {
+        return (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0"));
+    }
+
+    private void Jump()
+    {
+        jumps++;
+        yVelocity = jumpHeight;
+    }
+}
